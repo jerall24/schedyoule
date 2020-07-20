355d55441902ab5a7165e011cca91d6a3329c935
carinaramos <cgramos50@gmail.com>
2020-07-20 15:00:50 -0400

    form placeholders + validations

--- app/website.py
+++ app/website.py
@@ -89,71 +89,32 @@ def add_entry():
     if not session.get("logged_in"):
         abort(401)
 
-    # TO DO BEFORE UNCOMMENTING
-    # 1. fix date formats for created_date, end_date, requested_start_date
-    # 2.turn empty --> 0s for career_level_from, career_level_to
+    # POSSIBLE ISSUES
+    # 1. numeric values (assignment_id, career_level_from, career_level_to, candidates)
+    # 2. date formats yyyy-mm-dd (created_date, requested_start_date, end_date)
+    # 3. boolean formats (sold (Yes/No), gcp_preference (Y/N), client_contract_based (Yes/No))
 
-    # new_entry = models.Role(int(request.form["assignment_id"]), request.form["demand_type"], \
-    #             request.form["career_track"], request.form["location_radius"], request.form["source_location"], \
-    #             request.form["gu"], request.form["assignment_fulfillment_entity"], request.form["client"], \
-    #             request.form["project"], request.form["project_metro_city"], request.form["project_supervising_entity"], \
-    #             request.form["project_client_supply_demand_specialist"], request.form["sold"] == "Yes", \
-    #             request.form["gcp_preference"] == "Y", request.form["client_contract_based"] == "Yes", \
-    #             request.form["assignment_title"], request.form["description"], request.form["fulfillment_channel"], \
-    #             request.form["created_date"], request.form["source"], request.form["requested_start_date"], \
-    #             request.form["end_date"], request.form["status"], int(request.form["career_level_from"]), \
-    #             int(request.form["career_level_to"]), request.form["talent_segment"], request.form["assigned_role"], \
-    #             request.form["work_location"], \
-    #             request.form["assignment_primary_specialization_level_one"], \
-    #             request.form["assignment_primary_specialization_level_two"], \
-    #             request.form["assignment_primary_specialization_level_three"], \
-    #             request.form["assignment_primary_specialization_level_four"], \
-    #             request.form["assignment_primary_specialization_level_five"], \
-    #             request.form["assignment_primary_specialization_paths"], \
-    #             request.form["skill_and_proficiency"], request.form["primary_contact"], \
-    #             request.form["assignment_audit"], request.form["role_client_supply_demand_specialist"], \
-    #             int(request.form["candidates"]))
-    for row in range(3, 2842):
-        new_entry = models.Role(int(sheet.cell_value(row, 0)), \
-                    sheet.cell_value(row, 1), \
-                    sheet.cell_value(row, 2), \
-                    sheet.cell_value(row, 3), \
-                    sheet.cell_value(row, 4), \
-                    sheet.cell_value(row, 5), \
-                    sheet.cell_value(row, 6), \
-                    sheet.cell_value(row, 7), \
-                    sheet.cell_value(row, 8), \
-                    sheet.cell_value(row, 9), \
-                    sheet.cell_value(row, 10), \
-                    sheet.cell_value(row, 11), \
-                    sheet.cell_value(row, 12) == "Yes", \
-                    sheet.cell_value(row, 13)== "Y", \
-                    sheet.cell_value(row, 14)== "Yes", \
-                    sheet.cell_value(row, 15), \
-                    sheet.cell_value(row, 16), \
-                    sheet.cell_value(row, 17), \
-                    sheet.cell_value(row, 18), \
-                    sheet.cell_value(row, 19), \
-                    sheet.cell_value(row, 20), \
-                    sheet.cell_value(row, 21), \
-                    sheet.cell_value(row, 22), \
-                    int(sheet.cell_value(row, 23)), \
-                    int(sheet.cell_value(row, 24)), \
-                    sheet.cell_value(row, 25), \
-                    sheet.cell_value(row, 26), \
-                    sheet.cell_value(row, 27), \
-                    sheet.cell_value(row, 28), \
-                    sheet.cell_value(row, 29), \
-                    sheet.cell_value(row, 30), \
-                    sheet.cell_value(row, 31), \
-                    sheet.cell_value(row, 32), \
-                    sheet.cell_value(row, 33), \
-                    sheet.cell_value(row, 34), \
-                    sheet.cell_value(row, 35), \
-                    sheet.cell_value(row, 36), \
-                    sheet.cell_value(row, 37), \
-                    int(sheet.cell_value(row, 38)))
-        db.session.add(new_entry)
+    new_entry = models.Role(int(request.form["assignment_id"]), request.form["demand_type"], \
+                request.form["career_track"], request.form["location_radius"], request.form["source_location"], \
+                request.form["gu"], request.form["assignment_fulfillment_entity"], request.form["client"], \
+                request.form["project"], request.form["project_metro_city"], request.form["project_supervising_entity"], \
+                request.form["project_client_supply_demand_specialist"], request.form["sold"] == "Yes", \
+                request.form["gcp_preference"] == "Y", request.form["client_contract_based"] == "Yes", \
+                request.form["assignment_title"], request.form["description"], request.form["fulfillment_channel"], \
+                request.form["created_date"], request.form["source"], request.form["requested_start_date"], \
+                request.form["end_date"], request.form["status"], int(request.form["career_level_from"]), \
+                int(request.form["career_level_to"]), request.form["talent_segment"], request.form["assigned_role"], \
+                request.form["work_location"], \
+                request.form["assignment_primary_specialization_level_one"], \
+                request.form["assignment_primary_specialization_level_two"], \
+                request.form["assignment_primary_specialization_level_three"], \
+                request.form["assignment_primary_specialization_level_four"], \
+                request.form["assignment_primary_specialization_level_five"], \
+                request.form["assignment_primary_specialization_paths"], \
+                request.form["skill_and_proficiency"], request.form["primary_contact"], \
+                request.form["assignment_audit"], request.form["role_client_supply_demand_specialist"], \
+                int(request.form["candidates"]))
+    db.session.add(new_entry)
     db.session.commit()
     
     return redirect(url_for("search"))
--- xlsx_to_db.py
+++ xlsx_to_db.py
@@ -9,50 +9,51 @@ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0) 
 
 
-# just row 3, called row 2 here
-# for row in range(2, 3):
-#     new_entry = models.Role(int(sheet.cell_value(row, 0)), \ 
-#                 sheet.cell_value(row, 1), \
-#                 sheet.cell_value(row, 2), \
-#                 sheet.cell_value(row, 3), \
-#                 sheet.cell_value(row, 4), \
-#                 sheet.cell_value(row, 5), \
-#                 sheet.cell_value(row, 6), \
-#                 sheet.cell_value(row, 7), \ 
-#                 sheet.cell_value(row, 8), \
-#                 sheet.cell_value(row, 9), \ 
-#                 sheet.cell_value(row, 10), \
-#                 sheet.cell_value(row, 11), \
-#                 sheet.cell_value(row, 12) == "Yes", \ 
-#                 sheet.cell_value(row, 13)== "Y", \
-#                 sheet.cell_value(row, 14)== "Yes", \
-#                 sheet.cell_value(row, 15), \
-#                 sheet.cell_value(row, 16), \
-#                 sheet.cell_value(row, 17), \
-#                 sheet.cell_value(row, 18), \
-#                 sheet.cell_value(row, 19), \
-#                 sheet.cell_value(row, 20), \
-#                 sheet.cell_value(row, 21), \
-#                 sheet.cell_value(row, 22), \
-#                 int(sheet.cell_value(row, 23)), \
-#                 int(sheet.cell_value(row, 24)), \
-#                 sheet.cell_value(row, 25), \
-#                 sheet.cell_value(row, 26), \
-#                 sheet.cell_value(row, 27), \
-#                 sheet.cell_value(row, 28), \
-#                 sheet.cell_value(row, 29), \ 
-#                 sheet.cell_value(row, 30), \
-#                 sheet.cell_value(row, 31), \
-#                 sheet.cell_value(row, 32), \
-#                 sheet.cell_value(row, 33), \
-#                 sheet.cell_value(row, 34), \
-#                 sheet.cell_value(row, 35), \
-#                 sheet.cell_value(row, 36), \
-#                 sheet.cell_value(row, 37), \
-#                 int(sheet.cell_value(row, 38)))
-# db.session.add(new_entry)
+# loop through all rows to read in
+for row in range(2, 3):
+    new_entry = models.Role(int(sheet.cell_value(row, 0)), \ 
+                sheet.cell_value(row, 1), \
+                sheet.cell_value(row, 2), \
+                sheet.cell_value(row, 3), \
+                sheet.cell_value(row, 4), \
+                sheet.cell_value(row, 5), \
+                sheet.cell_value(row, 6), \
+                sheet.cell_value(row, 7), \ 
+                sheet.cell_value(row, 8), \
+                sheet.cell_value(row, 9), \ 
+                sheet.cell_value(row, 10), \
+                sheet.cell_value(row, 11), \
+                sheet.cell_value(row, 12) == "Yes", \ 
+                sheet.cell_value(row, 13)== "Y", \
+                sheet.cell_value(row, 14)== "Yes", \
+                sheet.cell_value(row, 15), \
+                sheet.cell_value(row, 16), \
+                sheet.cell_value(row, 17), \
+                sheet.cell_value(row, 18), \
+                sheet.cell_value(row, 19), \
+                sheet.cell_value(row, 20), \
+                sheet.cell_value(row, 21), \
+                sheet.cell_value(row, 22), \
+                int(sheet.cell_value(row, 23)), \
+                int(sheet.cell_value(row, 24)), \
+                sheet.cell_value(row, 25), \
+                sheet.cell_value(row, 26), \
+                sheet.cell_value(row, 27), \
+                sheet.cell_value(row, 28), \
+                sheet.cell_value(row, 29), \ 
+                sheet.cell_value(row, 30), \
+                sheet.cell_value(row, 31), \
+                sheet.cell_value(row, 32), \
+                sheet.cell_value(row, 33), \
+                sheet.cell_value(row, 34), \
+                sheet.cell_value(row, 35), \
+                sheet.cell_value(row, 36), \
+                sheet.cell_value(row, 37), \
+                int(sheet.cell_value(row, 38)))
+#   db.session.add(new_entry)
 # db.session.commit()
 
+# EXAMPLE
 # new_entry = models.Role(3145054, \
 #             "New", \
 #             "", \
