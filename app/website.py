c1c42927c76ff071a991cc523bc54bc417df0e57
carinaramos <cgramos50@gmail.com>
2020-07-20 14:24:29 -0400

    add row 2

--- app/website.py
+++ app/website.py
@@ -7,6 +7,15 @@ from flask import Flask, request, session, g, redirect, url_for, \
                   abort, render_template, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+# Reading an excel file using Python 
+import xlrd 
+  
+# Give the location of the file 
+loc = ("position_descriptions.xlsx") 
+  
+# To open Workbook 
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
 
 # get the folder where this file runs
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -79,34 +88,21 @@ def add_entry():
     """Adds new post to the database."""
     if not session.get("logged_in"):
         abort(401)
-    new_entry = models.Role(3145054, "New", "", \
-                "Any Location Globally", "Hong Kong", "Greater China", \
-                "", "UBS", "UBS AG Zürich - Securities", \
-                "Hong Kong", "Financial Services > FS Europe OU > FS Europe ASGR", \
-                "David,Frederick Ryan C.", True, \
-                True, False, "Spring Application Framework-11-Manila", \
-                "Act as software detectives, provide a dynamic service identifying and solving issues within multiple components of critical business systems.", \
-                "IDC/PDC Scheduling", '2019-11-21', "ABA", \
-                '2020-05-03', '2021-03-30', "Open - In Process", 0, \
-                0, "Software Engineering", "Application Support Engineer", "Hong Kong", \
-                "Technology", \
-                "No Technology Category", \
-                "No Technology Specialization", \
-                "", \
-                "", \
-                "Technology > No Technology Category > No Technology Specialization", \
-                " 1 - Java Enterprise Edition (P3 - Advanced) | 2 - Spring Application Framework (P3 - Advanced)", "Aquino,Carlo Antonio Soriano", "", \
-                "David,Frederick Ryan C.", 0)
-    # new_entry = models.Role(request.form["assignment_id"], request.form["demand_type"], \
+
+    # TO DO BEFORE UNCOMMENTING
+    # 1. fix date formats for created_date, end_date, requested_start_date
+    # 2.turn empty --> 0s for career_level_from, career_level_to
+
+    # new_entry = models.Role(int(request.form["assignment_id"]), request.form["demand_type"], \
     #             request.form["career_track"], request.form["location_radius"], request.form["source_location"], \
     #             request.form["gu"], request.form["assignment_fulfillment_entity"], request.form["client"], \
     #             request.form["project"], request.form["project_metro_city"], request.form["project_supervising_entity"], \
-    #             request.form["project_client_supply_demand_specialist"], request.form["sold"], \
-    #             request.form["gcp_preference"], request.form["client_contract_based"], \
+    #             request.form["project_client_supply_demand_specialist"], request.form["sold"] == "Yes", \
+    #             request.form["gcp_preference"] == "Y", request.form["client_contract_based"] == "Yes", \
     #             request.form["assignment_title"], request.form["description"], request.form["fulfillment_channel"], \
     #             request.form["created_date"], request.form["source"], request.form["requested_start_date"], \
-    #             request.form["end_date"], request.form["status"], request.form["career_level_from"], \
-    #             request.form["career_level_to"], request.form["talent_segment"], request.form["assigned_role"], \
+    #             request.form["end_date"], request.form["status"], int(request.form["career_level_from"]), \
+    #             int(request.form["career_level_to"]), request.form["talent_segment"], request.form["assigned_role"], \
     #             request.form["work_location"], \
     #             request.form["assignment_primary_specialization_level_one"], \
     #             request.form["assignment_primary_specialization_level_two"], \
@@ -116,9 +112,51 @@ def add_entry():
     #             request.form["assignment_primary_specialization_paths"], \
     #             request.form["skill_and_proficiency"], request.form["primary_contact"], \
     #             request.form["assignment_audit"], request.form["role_client_supply_demand_specialist"], \
-    #             request.form["candidates"])
+    #             int(request.form["candidates"]))
+    for row in range(2, 3):
+        new_entry = models.Role(int(sheet.cell_value(row, 0)), \
+                    sheet.cell_value(row, 1), \
+                    sheet.cell_value(row, 2), \
+                    sheet.cell_value(row, 3), \
+                    sheet.cell_value(row, 4), \
+                    sheet.cell_value(row, 5), \
+                    sheet.cell_value(row, 6), \
+                    sheet.cell_value(row, 7), \
+                    sheet.cell_value(row, 8), \
+                    sheet.cell_value(row, 9), \
+                    sheet.cell_value(row, 10), \
+                    sheet.cell_value(row, 11), \
+                    sheet.cell_value(row, 12) == "Yes", \
+                    sheet.cell_value(row, 13)== "Y", \
+                    sheet.cell_value(row, 14)== "Yes", \
+                    sheet.cell_value(row, 15), \
+                    sheet.cell_value(row, 16), \
+                    sheet.cell_value(row, 17), \
+                    sheet.cell_value(row, 18), \
+                    sheet.cell_value(row, 19), \
+                    sheet.cell_value(row, 20), \
+                    sheet.cell_value(row, 21), \
+                    sheet.cell_value(row, 22), \
+                    int(sheet.cell_value(row, 23)), \
+                    int(sheet.cell_value(row, 24)), \
+                    sheet.cell_value(row, 25), \
+                    sheet.cell_value(row, 26), \
+                    sheet.cell_value(row, 27), \
+                    sheet.cell_value(row, 28), \
+                    sheet.cell_value(row, 29), \
+                    sheet.cell_value(row, 30), \
+                    sheet.cell_value(row, 31), \
+                    sheet.cell_value(row, 32), \
+                    sheet.cell_value(row, 33), \
+                    sheet.cell_value(row, 34), \
+                    sheet.cell_value(row, 35), \
+                    sheet.cell_value(row, 36), \
+                    sheet.cell_value(row, 37), \
+                    int(sheet.cell_value(row, 38)))
+    
     db.session.add(new_entry)
     db.session.commit()
+    
     return redirect(url_for("search"))
 
 
--- xlsx_to_db.py
+++ xlsx_to_db.py
@@ -7,10 +7,77 @@ loc = ("position_descriptions.xlsx")
 # To open Workbook 
 wb = xlrd.open_workbook(loc) 
 sheet = wb.sheet_by_index(0) 
-  
-# For row 0 and column 0 
-print(sheet.cell_value(0, 0)) 
 
-# new_entry = models.Role(request.form["title"], request.form["text"])
-#     db.session.add(new_entry)
-#     db.session.commit()
+
+# just row 3, called row 2 here
+# for row in range(2, 3):
+#     new_entry = models.Role(int(sheet.cell_value(row, 0)), \ 
+#                 sheet.cell_value(row, 1), \
+#                 sheet.cell_value(row, 2), \
+#                 sheet.cell_value(row, 3), \
+#                 sheet.cell_value(row, 4), \
+#                 sheet.cell_value(row, 5), \
+#                 sheet.cell_value(row, 6), \
+#                 sheet.cell_value(row, 7), \ 
+#                 sheet.cell_value(row, 8), \
+#                 sheet.cell_value(row, 9), \ 
+#                 sheet.cell_value(row, 10), \
+#                 sheet.cell_value(row, 11), \
+#                 sheet.cell_value(row, 12) == "Yes", \ 
+#                 sheet.cell_value(row, 13)== "Y", \
+#                 sheet.cell_value(row, 14)== "Yes", \
+#                 sheet.cell_value(row, 15), \
+#                 sheet.cell_value(row, 16), \
+#                 sheet.cell_value(row, 17), \
+#                 sheet.cell_value(row, 18), \
+#                 sheet.cell_value(row, 19), \
+#                 sheet.cell_value(row, 20), \
+#                 sheet.cell_value(row, 21), \
+#                 sheet.cell_value(row, 22), \
+#                 int(sheet.cell_value(row, 23)), \
+#                 int(sheet.cell_value(row, 24)), \
+#                 sheet.cell_value(row, 25), \
+#                 sheet.cell_value(row, 26), \
+#                 sheet.cell_value(row, 27), \
+#                 sheet.cell_value(row, 28), \
+#                 sheet.cell_value(row, 29), \ 
+#                 sheet.cell_value(row, 30), \
+#                 sheet.cell_value(row, 31), \
+#                 sheet.cell_value(row, 32), \
+#                 sheet.cell_value(row, 33), \
+#                 sheet.cell_value(row, 34), \
+#                 sheet.cell_value(row, 35), \
+#                 sheet.cell_value(row, 36), \
+#                 sheet.cell_value(row, 37), \
+#                 int(sheet.cell_value(row, 38)))
+# db.session.add(new_entry)
+# db.session.commit()
+
+# new_entry = models.Role(3145054, \
+#             "New", \
+#             "", \
+#             "Any Location Globally", \
+#             "Hong Kong", \
+#             "Greater China", \
+#             "", \
+#             "UBS", \
+#             "UBS AG Zürich - Securities", \
+#             "Hong Kong", \
+#             "Financial Services > FS Europe OU > FS Europe ASGR", \
+#             "David,Frederick Ryan C.", \
+#             True, True, False, \
+#             "Spring Application Framework-11-Manila", \
+#             "Act as software detectives, provide a dynamic service identifying and solving issues within multiple components of critical business systems.", \
+#             "IDC/PDC Scheduling", '2019-11-21', "ABA", \
+#             '2020-05-03', '2021-03-30', "Open - In Process", \
+#             0, 0, \
+#             "Software Engineering", "Application Support Engineer", "Hong Kong", \
+#             "Technology", \
+#             "No Technology Category", \
+#             "No Technology Specialization", \
+#             "", \
+#             "", \
+#             "Technology > No Technology Category > No Technology Specialization", \
+#             " 1 - Java Enterprise Edition (P3 - Advanced) | 2 - Spring Application Framework (P3 - Advanced)", "Aquino,Carlo Antonio Soriano", "", \
+#             "David,Frederick Ryan C.", \
+#             0)
