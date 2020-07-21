a6ffef277e03dea2c46b721bcbc99e234c8fd68b
carinaramos <cgramos50@gmail.com>
2020-07-21 01:04:47 -0400

    table search logging

--- app/static/main.js
+++ app/static/main.js
@@ -32,7 +32,7 @@
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-  
+    console.log("entering loop")
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
       for (j = 0; j < 5; i++) {
